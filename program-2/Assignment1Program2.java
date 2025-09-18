99b136aae3d55a20f8cb414e434df2e0069483d4
adam-sheridan <asheridan@hartford.edu>
2025-09-17 22:35:26 -0400

    Corrections were made

--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -1,8 +1,8 @@
 public class Assignment1Program2 {
   public static void main(String[] args) {
-    int number = 165;
+    int number = 45;
     int radix = 4;
     String base4Number = Integer.toString(number, radix);
-    System.out.println("The base 4 number converted from 165 base 10 is:" + base4Number);
+    System.out.println("The base 4 number converted from 45 base 10 is: " + base4Number);
   }
 }
