20e38a8555ee5c32ad75e00af4e7da96b21edb79
adam-sheridan <asheridan@hartford.edu>
2025-09-17 22:36:27 -0400

    Comments were added and scan was closed

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -10,21 +10,30 @@ public class Assignment1Program1 {
     System.out.println("    AAAAAAAAAA    SSSSSS");
     System.out.println("  AA          AA        SS");
     System.out.println("AA              AA  SSSS");
-
+    //------------------------------------------------------
+    //5 character string
+    //------------------------------------------------------
     Scanner scan = new Scanner(System.in);
-    String input;
-    input = scan.next();
+    String input = scan.next();
     String chop = input.substring(1,5);
-    String reverse = ""+chop.charAt(2)+chop.charAt(1)+chop.charAt(0);
+    String reverse = "" + chop.charAt(2) + chop.charAt(1) + chop.charAt(0);
     System.out.println(reverse);
-
+    //------------------------------------------------------
+    //Temperature conversion
+    //------------------------------------------------------
     int fahrenheit = scan.nextInt();
-    int celsius = ((fahrenheit - 32) /(9/5));
+    int celsius = (int)((fahrenheit - 32) / 1.8);
     System.out.println(celsius);
-
+    //------------------------------------------------------
+    //Random number generator
+    //------------------------------------------------------
     Random rand = new Random();
     int num = rand.nextInt(16384 - 32 + 1) + 32;
     System.out.println(num);
+    //------------------------------------------------------
+    //Final sequence
+    //------------------------------------------------------
     System.out.println(celsius + reverse + num);
+    scan.close();
     }
 }
